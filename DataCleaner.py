f77667b0e25add9900191b115afa345aa4456398
Aleksa V <viganjg12@gmail.com>
2023-03-26 01:30:27 -0400

    did shit

---
+++ DataCleaner.py
@@ -0,0 +1,61 @@
+import os.path as path
+from sklearn.preprocessing import LabelEncoder
+from typing import Tuple
+
+import pandas as pd
+
+BASE_DIRECTORY = path.abspath(path.dirname('Assignment2'))
+DATAFOLDER = path.join(BASE_DIRECTORY, 'Data')
+DATAFILE = path.join(DATAFOLDER, 'waveform-+noise.csv')
+
+
+class DataCleaner:
+
+    def getAttributesAndFeatures(self) -> Tuple[pd.DataFrame, pd.DataFrame]:
+
+        print(BASE_DIRECTORY)
+        print(DATAFOLDER)
+
+        if path.exists(DATAFILE):
+            print(f'File {DATAFILE} found.... cleaning')
+            csvDataFrame: pd.DataFrame = pd.DataFrame(pd.read_csv(DATAFILE))
+
+            # append simple column names
+            csvDataFrame.columns = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41']
+
+            # csvDataFrameEncoded = self.encodeDataToNumerical(csvDataFrame)
+
+            # convert non-numerical fields into numerical ones
+            # csvDataFrameEncoded = MultiColumnLabelEncoder().fit_transform(csvDataFrame)
+
+            labelFrame, featuresFrame = self.cleanData(csvDataFrame)
+            return labelFrame, featuresFrame
+        else:
+            raise FileNotFoundError(f'File {DATAFILE} not found.')
+
+    def cleanData(self, dataframe: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        labelFrame = dataframe.iloc[:, 40:41]  # Select the 41st column for feature
+        featuresFrame = dataframe.iloc[:, :40]  # Select the rest of the columns for attributes
+        return labelFrame, featuresFrame
+
+    def encodeDataToNumerical(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+
+        labelsToLoop = dataframe.columns
+        le = LabelEncoder()
+
+        for label in labelsToLoop:
+            encodedColumnContents = le.fit_transform(dataframe[label])
+            dataframe.drop(label, axis=1, inplace=True)
+            dataframe[label] = encodedColumnContents
+
+        return dataframe
+
+# if __name__ == "__main__":
+#     # pd.set_option('display.max_columns', 50)
+#     # pd.set_option('display.max_colwidth', 100)
+#     #
+#     # featureFrame, attributeFrame = DataCleaner().getAttributesAndFeatures()
+#     # print("Statistics on the feature")
+#     # print(featureFrame.describe(include='all'))
+#     # print("Statistics on the attributes")
+#     # print(attributeFrame.describe(include='all'))
--- main.py
+++ main.py
@@ -1,21 +1,54 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score, train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
+
+from DataCleaner import DataCleaner
+
+
 def pipeline():
     # example with cross validation
-
-    from sklearn.ensemble import RandomForestClassifier
-    from sklearn.model_selection import cross_val_score
-    import numpy as np
-
     # Generate mock dataset
-    X = np.random.rand(100, 10)  # 100 samples with 10 features each
-    y = np.random.randint(2, size=100)  # Binary labels (0 or 1)
 
-    # Train random forest classifier with cross-validation
+    labelFrame, features = DataCleaner().getAttributesAndFeatures()
+    # print(features.shape)
+    # print(labelFrame.shape)
+    # print(labelFrame.head())
+
+    X_train, X_test, y_train, y_test = train_test_split(features, labelFrame, test_size=0.2, random_state=42)
+    y_train = np.ravel(y_train)
+    y_test = np.ravel(y_test)
+
+    # print(X_train.shape)
+    # print(X_test.shape)
+    # print(y_train.shape)
+    # print(y_test.shape)
+
+    # Train random forest classifier
     rf = RandomForestClassifier(n_estimators=100, random_state=42)
-    scores = cross_val_score(rf, X, y, cv=5)  # 5-fold cross-validation
+    rf.fit(X_train, y_train)
 
-    # Print average accuracy and standard deviation across folds
-    print("Accuracy:", np.mean(scores))
-    print("Standard deviation:", np.std(scores))
+    # Predict on testing set
+    y_pred = rf.predict(X_test)
+
+    # Evaluate accuracy
+    accuracy = accuracy_score(y_test, y_pred)
+    print("Accuracy:", accuracy)
+
+    # plot the feature importance to file
+    importances = rf.feature_importances_
+    std = np.std([tree.feature_importances_ for tree in rf.estimators_], axis=0)
+    featureslist = features.columns.values.tolist()
+    forest_importances = pd.Series(importances, index=featureslist)
+    fig, ax = plt.subplots()
+    forest_importances.plot.bar(yerr=std, ax=ax)
+    ax.set_title("Feature importances using MDI")
+    ax.set_ylabel("Mean decrease in impurity")
+    fig.tight_layout()
+
+    plt.savefig('featureImportancePlot.png')
 
 if __name__ == '__main__':
     pipeline()
